2cfa85ed56b08f698047625d70eb0c1ddc48faef
Benjamín de Jesús Cueva Niño de Guzmán <cueva_dev@hotmail.com>
2026-04-05 15:16:10 -0500

    hola

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel(),
+  output: "server",
 });
